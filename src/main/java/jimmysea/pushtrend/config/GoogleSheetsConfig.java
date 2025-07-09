b9033043c9539d82b755d48e81c0298c04202a6c
thao <thaont124@outlook.com.vn>
2025-07-09 09:42:07 +0700

    update docker

--- src/main/java/jimmysea/pushtrend/config/GoogleSheetsConfig.java
+++ src/main/java/jimmysea/pushtrend/config/GoogleSheetsConfig.java
@@ -6,9 +6,11 @@ import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.GeneralSecurityException;
@@ -19,21 +21,25 @@ public class GoogleSheetsConfig {
 
     private static final String APPLICATION_NAME = "PushTrend";
 
+    @Value("${google.credentials.localpath}")
+    private String credentialsPath;
+
     @Bean
     public Sheets googleSheetsServiceConfig() throws IOException, GeneralSecurityException {
-        InputStream credentialsStream = getClass().getClassLoader().getResourceAsStream("credentials.json");
-        if (credentialsStream == null) {
-            throw new IOException("Credentials file not found in resources");
+        try (InputStream credentialsStream = new FileInputStream(credentialsPath)) {
+            if (credentialsStream == null) {
+                throw new IOException("Credentials file not found at " + credentialsPath);
+            }
+
+            GoogleCredentials credentials = GoogleCredentials.fromStream(credentialsStream)
+                    .createScoped(Collections.singleton(SheetsScopes.SPREADSHEETS_READONLY));
+
+            return new Sheets.Builder(
+                    GoogleNetHttpTransport.newTrustedTransport(),
+                    JacksonFactory.getDefaultInstance(),
+                    new HttpCredentialsAdapter(credentials))
+                    .setApplicationName(APPLICATION_NAME)
+                    .build();
         }
-
-        GoogleCredentials credentials = GoogleCredentials.fromStream(credentialsStream)
-                .createScoped(Collections.singleton(SheetsScopes.SPREADSHEETS_READONLY));
-
-        return new Sheets.Builder(
-                GoogleNetHttpTransport.newTrustedTransport(),
-                JacksonFactory.getDefaultInstance(),
-                new HttpCredentialsAdapter(credentials))
-                .setApplicationName(APPLICATION_NAME)
-                .build();
     }
 }
